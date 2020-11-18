0deb08f682353608b44948955d9bf29488a0694e
Roy Fu <lingoerer@gmail.com>
2020-11-18 17:12:25 +0800

    chore: disable tls untill xCode fix the inner xcframework code sign bug

--- Package.swift
+++ Package.swift
@@ -11,12 +11,17 @@ let package = Package(
             targets: ["Artemisia"]),
     ],
     dependencies: [
-        .package(name: "OpenSSL", url: "https://github.com/lingoer/openssl-xcframeworks.git", from: "1.1.1")
+        /*.package(name: "OpenSSL", url: "https://github.com/lingoer/openssl-xcframeworks.git", from: "1.1.1")*/
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "MosquittoC", dependencies: [.byName(name: "OpenSSL")], cSettings:[.define("WITH_THREADING"), .define("WITH_TLS")]),
+        .target(name: "MosquittoC", dependencies: [
+//                .byName(name: "OpenSSL")
+                ], cSettings:[
+                    .define("WITH_THREADING"),
+                    /*.define("WITH_TLS"),*/
+                ]),
         .target(
             name: "Artemisia",
             dependencies: ["MosquittoC"]),

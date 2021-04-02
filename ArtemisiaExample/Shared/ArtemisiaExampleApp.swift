2e2ecd4b3e2ccfa5097b6c8a4c5356fd2a877df4
Roy Fu <lingoerer@gmail.com>
2021-04-02 13:23:55 +0800

    feat: update example

--- ArtemisiaExample/Shared/ArtemisiaExampleApp.swift
+++ ArtemisiaExample/Shared/ArtemisiaExampleApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct ArtemisiaExampleApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
+
--- ArtemisiaExample/Shared/ContentView.swift
+++ ArtemisiaExample/Shared/ContentView.swift
@@ -9,67 +9,71 @@ import SwiftUI
 import Combine
 import Artemisia
 
-struct Model: Codable {
-    let sender: String
-    let text: String
+struct Model: Codable, Identifiable {
+  let sender: String
+  let text: String
+  var id: String { text + sender }
 }
 
 extension Model: MQTTTransmittable {
-    func toMessage(options defaultOptions: Mosquitto.PublishOptions) -> Mosquitto.Message {
-        .message(try! JSONEncoder().encode(self), options: defaultOptions)
-//        try! JSONEncoder().encode(self).toMessage(options: defaultOptions)
-    }
-    static func fromMessage(message: Mosquitto.Message) -> Model {
-        try! JSONDecoder().decode(self, from: message.payload)
-    }
+  func toMessage(options defaultOptions: Mosquitto.PublishOptions) -> Mosquitto.Message {
+    .message(try! JSONEncoder().encode(self), options: defaultOptions)
+  }
+  static func fromMessage(message: Mosquitto.Message) -> Model {
+    try! JSONDecoder().decode(self, from: message.payload)
+  }
 }
 
 class ViewModel: ObservableObject {
-
-    let client = Artemisia.connect(host: "broker.emqx.io")
-    @Published var receivedItems: [Model] = []
-    var text: String = ""
-    var bag:[AnyCancellable] = []
-    let label:String
+  
+  let client = Artemisia.connect(host: "broker.emqx.io", version: .v5)
+  @Published
+  var receivedItems: [Model] = []
+  var text: String = ""
+  var bag:[AnyCancellable] = []
+  let label:String
+  
+  init(label: String) {
+    self.label = label
+    client[label]
+      .receive(on: RunLoop.main)
+      .sink {[weak self] (item:Model) in
+        self?.receivedItems.append(item)
+      }.store(in: &bag)
     
-    init(label: String) {
-        self.label = label
-        client[label].sink {[self] (item:Model) in
-            receivedItems.append(item)
-        }.store(in: &bag)
-
-    }
-    func send() {
-        let d = try! JSONEncoder().encode(Model(sender: label, text: text))
-        let s = String(data: d, encoding: .utf8)!
-        print(s)
-        client[label].publish(message: s)
-    }
+  }
+  func send() {
+    client[label].publish(message: Model(sender: "artemisia", text: text))
+    text = ""
+  }
 }
 
 struct ContentView: View {
-    @StateObject var viewmodel: ViewModel = ViewModel(label: "aemaeth/topicit")
-    
-    var body: some View {
-        VStack(alignment: .center, content: {
-            List(viewmodel.receivedItems, id: \.text) { (item:Model) in
-                HStack(alignment: .center, content: {
-                    Text(item.text)
-                    Text(item.sender)
-                })
-            }
-            HStack(alignment: .center, content: {
-                TextField("Text to send", text: $viewmodel.text)
-                Button(action: viewmodel.send, label: {
-                    Text("Send")
-                })
-            })
-        }).textFieldStyle(RoundedBorderTextFieldStyle())
-    }
+  @StateObject
+  var viewmodel: ViewModel = ViewModel(label: "aemaeth/topicit")
+  
+  var body: some View {
+    VStack(alignment: .center, content: {
+      List(viewmodel.receivedItems) { (item:Model) in
+        HStack(alignment: .center, content: {
+          Text("\(item.sender): \(item.text)")
+        })
+      }
+      HStack(alignment: .center, content: {
+        TextField("Text to send", text: $viewmodel.text)
+        Button(action: viewmodel.send, label: {
+          Text("Send")
+        })
+      }).padding()
+    }).frame(minWidth: 600, idealWidth: 600, maxWidth: .infinity, minHeight: 400, idealHeight: 400, maxHeight: .infinity, alignment: .center)
+    .textFieldStyle(RoundedBorderTextFieldStyle())
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+      .previewLayout(.sizeThatFits)
+  }
 }
+
